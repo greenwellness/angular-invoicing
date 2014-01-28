65fe05524097c33ebef6d81779a5a1df5be8326a
supersymmetry <maildumpert@gmail.com>
2014-01-28 09:42:48 +0100

    init

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ function InvoiceController($scope) {
   $scope.printMode = false;
 
   var sample_invoice = {
-            tax: 13.00, 
-            invoice_number: 10,
+            tax: 21.00,
+            invoice_number: 1100000001,
             customer_info:  {name: "Mr. John Doe", web_link: "John Doe Designs Inc.", address1: "1 Infinite Loop", address2: "Cupertino, California, US", postal: "90210"},
-            company_info:  {name: "Metaware Labs", web_link: "www.metawarelabs.com", address1: "123 Yonge Street", address2: "Toronto, ON, Canada", postal: "M5S 1B6"},
-              items:[ {qty:10, description:'Gadget', cost:9.95}]};
+            company_info:  {name: "Green Wellness", web_link: "https://www.greenwellness.nl", address1: "NS Plein 65, gebouw 88", address2: "Tilburg, The Netherlands", postal: "5014 DC"},
+              items:[ {qty:10, description:'Wellnessbonnen', cost:10.00}]};
 
   if(localStorage["invoice"] == "" || localStorage["invoice"] == null){
     $scope.invoice = sample_invoice;
@@ -17,16 +17,16 @@ function InvoiceController($scope) {
     $scope.invoice =  JSON.parse(localStorage["invoice"]);
   }
     $scope.addItem = function() {
-        $scope.invoice.items.push({qty:0, cost:0, description:""});    
+        $scope.invoice.items.push({qty:0, cost:0, description:""});
     }
     $scope.removeLogo = function(element) {
         var elem = angular.element("#remove_logo");
-        if(elem.text() == "Show Logo"){
-          elem.text("Remove Logo");
+        if(elem.text() == "toon"){
+          elem.text("verwijder");
           $scope.logoRemoved = false;
         }
         else{
-          elem.text("Show Logo");
+          elem.text("toon");
           $scope.logoRemoved = true;
         }
 
@@ -40,9 +40,9 @@ function InvoiceController($scope) {
         $scope.logoRemoved = false;
     }
     $scope.removeItem = function(item) {
-        $scope.invoice.items.splice($scope.invoice.items.indexOf(item), 1);    
+        $scope.invoice.items.splice($scope.invoice.items.indexOf(item), 1);
     }
-    
+
     $scope.invoice_sub_total = function() {
         var total = 0.00;
         angular.forEach($scope.invoice.items, function(item, key){
@@ -56,14 +56,14 @@ function InvoiceController($scope) {
     $scope.calculate_grand_total = function() {
         localStorage["invoice"] = JSON.stringify($scope.invoice);
         return $scope.calculate_tax() + $scope.invoice_sub_total();
-    } 
+    }
 
     $scope.printInfo = function() {
       window.print();
     }
 
     $scope.clearLocalStorage = function(){
-      var confirmClear = confirm("Are you sure you would like to clear the invoice?");
+      var confirmClear = confirm("Weet je zeker dat je de factuur wilt wissen? Dit is niet ongedaan te maken.");
       if(confirmClear){
         localStorage["invoice"] = "";
         $scope.invoice = sample_invoice;
@@ -73,10 +73,10 @@ function InvoiceController($scope) {
 
 };
 
-angular.module('jqanim', []).directive('jqAnimate', function(){ 
-  return function(scope, instanceElement){ 
-      setTimeout(function() {instanceElement.show('slow');}, 0); 
-  } 
+angular.module('jqanim', []).directive('jqAnimate', function(){
+  return function(scope, instanceElement){
+      setTimeout(function() {instanceElement.show('slow');}, 0);
+  }
 });
 
 function readURL(input) {
